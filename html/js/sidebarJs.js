6fe38e23b7cd3fd23e96831dbf116def2ed6ab47
Darpankumar Tank <pca43@server7.com>
2023-02-16 18:23:18 +0530

    Landing Page

---
+++ html/js/sidebarJs.js
@@ -0,0 +1,19 @@
+function openLeftFilterMenu() {
+    if(document.getElementById("leftFilterMenu").style.display== "block"){
+        document.getElementById("leftFilterMenu").style.display = "none";
+    }
+    else{
+        document.getElementById("leftFilterMenu").style.display = "block";
+    }
+  }
+  
+  function closeLeftFilterMenu() {
+    document.getElementById("leftFilterMenu").style.display = "none";
+  }
+  function openLeftMainMenu() {
+    document.getElementById("leftMainMenu").style.display = "block";
+  }
+  
+  function closeLeftMainMenu() {
+    document.getElementById("leftMainMenu").style.display = "none";
+  }
